eb076663d05362da6b2ba57bc331ec49f2e58f4a
liuxchao <287935799@qq.com>
2023-08-09 10:50:32 +0800

    avif

---
+++ android/app/src/main/java/com/example/avif_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.avif_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Runner/swifttest.swift
@@ -0,0 +1,8 @@
+//
+//  swifttest.swift
+//  Runner
+//
+//  Created by lxc on 2023/8/9.
+//
+
+import Foundation
